be167e52c77c8b0605871fdf75ed2d07b221ccde
Joni <153834656+JT84-bit@users.noreply.github.com>
2024-02-12 12:01:49 +0200

    Added numbers to ceasarcipher

--- functions.js
+++ functions.js
@@ -1,68 +1,157 @@
-function capitalize(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function reverseString(string){
-    let arrayString = string.split("");
-    arrayString = arrayString.reverse();
-    let newString = arrayString.join("");
-    return newString;
+function reverseString(string) {
+  let arrayString = string.split("");
+  arrayString = arrayString.reverse();
+  let newString = arrayString.join("");
+  return newString;
 }
 
 const calculator = {
-    add : (a ,b) =>{ return a + b;},
-    multiply : (a, b) => {return a * b},
-    substract : (a, b) => {return a - b},
-    divide : (a, b) => {return a / b}
-}
+  add: (a, b) => {
+    return a + b;
+  },
+  multiply: (a, b) => {
+    return a * b;
+  },
+  substract: (a, b) => {
+    return a - b;
+  },
+  divide: (a, b) => {
+    return a / b;
+  },
+};
 
 // Checks if letter is uppercase/lowercase and returns letter shifted for asked amount
-function shiftLetter(letter, shift){
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    const alphabetCapital = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    if(alphabet.includes(letter)){
-        let newShift = alphabet.indexOf(letter) + shift;
-        if(newShift < alphabet.length){
-            return alphabet[newShift];
-        }
-        newShift -= alphabet.length;
-        return alphabet[newShift];
-    } 
-    let newShift = alphabetCapital.indexOf(letter) + shift;
-    if(newShift < alphabetCapital.length){
-        return alphabetCapital[newShift];
+function shiftLetter(letter, shift) {
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const alphabetCapital = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  if (alphabet.includes(letter)) {
+    let newShift = alphabet.indexOf(letter) + shift;
+    if (newShift < alphabet.length) {
+      return alphabet[newShift];
     }
-    newShift -= alphabetCapital.length;
+    newShift -= alphabet.length;
+    return alphabet[newShift];
+  }
+  let newShift = alphabetCapital.indexOf(letter) + shift;
+  if (newShift < alphabetCapital.length) {
     return alphabetCapital[newShift];
+  }
+  newShift -= alphabetCapital.length;
+  return alphabetCapital[newShift];
 }
 
-function caesarCipher(string, shift){
-    const punctuation = [",", " ", ".", ":", "&", "%", "(", ")", "[", "]", "?", ";", "!"];
-    let newWord = "";
-    const wordArray = string.split("");
-    for(let i = 0;i < wordArray.length;i++){
-        if(punctuation.includes(wordArray[i]) || typeof wordArray[i] === 'number'){
-            newWord += wordArray[i];
-        } else {
-            let shifted = shiftLetter(wordArray[i], shift);
-            newWord += shifted;
-        }
+function caesarCipher(string, shift) {
+  const punctuation = [
+    ",",
+    " ",
+    ".",
+    ":",
+    "&",
+    "%",
+    "(",
+    ")",
+    "[",
+    "]",
+    "?",
+    ";",
+    "!",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+  ];
+  let newWord = "";
+  const wordArray = string.split("");
+  for (let i = 0; i < wordArray.length; i++) {
+    if (punctuation.includes(wordArray[i])) {
+      newWord += wordArray[i];
+    } else {
+      let shifted = shiftLetter(wordArray[i], shift);
+      newWord += shifted;
     }
-    return newWord;
+  }
+  return newWord;
 }
 
-function analyzeArray(numbers){
-    const sumOfArray = numbers.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue
-      },0);
-    const maxOfArray = numbers.reduce((a, b) => Math.max(a, b));
-    const minOfArray = numbers.reduce((a, b) => Math.min(a, b));
+function analyzeArray(numbers) {
+  const sumOfArray = numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  const maxOfArray = numbers.reduce((a, b) => Math.max(a, b));
+  const minOfArray = numbers.reduce((a, b) => Math.min(a, b));
 
-    const values = {sum: sumOfArray, max: maxOfArray, 
-        average: sumOfArray / numbers.length, length: numbers.length, min: minOfArray}
-    return values;
+  const values = {
+    sum: sumOfArray,
+    max: maxOfArray,
+    average: sumOfArray / numbers.length,
+    length: numbers.length,
+    min: minOfArray,
+  };
+  return values;
 }
 
-export {calculator, reverseString, capitalize, analyzeArray, caesarCipher}
-
-
+export { calculator, reverseString, capitalize, analyzeArray, caesarCipher };
--- functions.test.js
+++ functions.test.js
@@ -1,37 +1,49 @@
-import { calculator, capitalize, reverseString , analyzeArray ,caesarCipher} from "./functions";
+import {
+  calculator,
+  capitalize,
+  reverseString,
+  analyzeArray,
+  caesarCipher,
+} from "./functions";
 
-test('capitalize jack to be Jack', () => {
-    expect(capitalize('jack')).toBe('Jack');
-  });
+test("capitalize jack to be Jack", () => {
+  expect(capitalize("jack")).toBe("Jack");
+});
 
-test('reverse kirjastonhoitaja to be ajatiohnotsajrik ', () => {
-    expect(reverseString('kirjastonhoitaja')).toBe('ajatiohnotsajrik');
-  });
+test("reverse kirjastonhoitaja to be ajatiohnotsajrik ", () => {
+  expect(reverseString("kirjastonhoitaja")).toBe("ajatiohnotsajrik");
+});
 
 test('calculator add function to return 3 on "1 + 2" ', () => {
-    expect(calculator.add(1, 2)).toBe(3);
-  });
+  expect(calculator.add(1, 2)).toBe(3);
+});
 
 test('calculator multiply function to return 6 on "2 * 3" ', () => {
-    expect(calculator.multiply(2, 3)).toBe(6);
-  });
+  expect(calculator.multiply(2, 3)).toBe(6);
+});
 
 test('calculator divide function to return 3 on "6 / 2" ', () => {
-    expect(calculator.divide(6, 2)).toBe(3);
-  });
+  expect(calculator.divide(6, 2)).toBe(3);
+});
 
 test('calculator susbtract function to return 2 on "6 - 4" ', () => {
-    expect(calculator.substract(6, 4)).toBe(2);
-  });
+  expect(calculator.substract(6, 4)).toBe(2);
+});
 
-test('analyzeArray sum: 15, max: 5, average: 3, length: 5, min: 1', () => {
-    const numbers = [1, 2, 3, 4, 5]
-    expect(analyzeArray(numbers)).toEqual({sum: 15, max: 5, average: 3, length: 5, min: 1});
+test("analyzeArray sum: 15, max: 5, average: 3, length: 5, min: 1", () => {
+  const numbers = [1, 2, 3, 4, 5];
+  expect(analyzeArray(numbers)).toEqual({
+    sum: 15,
+    max: 5,
+    average: 3,
+    length: 5,
+    min: 1,
   });
+});
 
 test('ceasarCipher return "Ymj Tins uwtojhy Wzqjx!!" on input: "The Odin project Rules!!", 5 ', () => {
-    const numbers = [1, 2, 3, 4, 5]
-    expect(caesarCipher("The Odin project Rules!!", 5)).toBe("Ymj Tins uwtojhy Wzqjx!!");
-  });
-
-  
+  const numbers = [1, 2, 3, 4, 5];
+  expect(caesarCipher("The Odin project Rules!!", 5)).toBe(
+    "Ymj Tins uwtojhy Wzqjx!!"
+  );
+});
